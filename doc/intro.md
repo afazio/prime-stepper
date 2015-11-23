a82ac3fb7ed3ca37160c0dc3ebec17f4f62e6d13
Alfred J. Fazio <alfred.fazio@gmail.com>
2015-11-23 15:42:13 -0500

    remove unused stubs

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to prime-steps
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/prime_steps/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns prime-steps.core-test
-  (:require [clojure.test :refer :all]
-            [prime-steps.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
